e1833e029468b545ef435f5f6ff2331aa5d88c56
Alon Baker <66329736+RealSycoBak@users.noreply.github.com>
2024-12-20 13:01:44 -0500

    finishing touches and websockets

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,10 +3,28 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import * as jquery from "jquery"
 import "semantic-ui"
+import "channels"
+
+var scroll_bottom = function() {
+    if ($('#messages').length > 0) {
+        $('#messages').scrollTop($('#messages')[0].scrollHeight);
+    }
+}
+
+var submit_message = function() {
+    $('#messages_body').on('keydown', function(e) {
+        if(e.keyCode == 13) {
+            $('button').click();
+            e.target.value = "";
+        };
+    });
+};
 
 $(document).on('turbo:load', function () {
     console.log('loaded turbo links');
     $('.ui.dropdown').dropdown();
+    submit_message();
+    scroll_bottom();
 });
 
 // Use event delegation for the flash message close button
---
+++ app/javascript/channels/chatroom_channel.js
@@ -0,0 +1,21 @@
+import consumer from "channels/consumer"
+
+consumer.subscriptions.create("ChatroomChannel", {
+  connected() {
+    // Called when the subscription is ready for use on the server
+  },
+
+  disconnected() {
+    // Called when the subscription has been terminated by the server
+  },
+
+  received(data) {
+    var scroll_bottom = function() {
+      if ($('#messages').length > 0) {
+          $('#messages').scrollTop($('#messages')[0].scrollHeight);
+      }
+  }
+    $('#message-container').append(data.mod_message)
+    scroll_bottom();
+  }
+});
